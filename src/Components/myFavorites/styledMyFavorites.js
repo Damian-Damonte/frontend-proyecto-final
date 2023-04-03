afca27830a9cde9682c567ab9c0efce40169b9dc
damiandamonte <damontedamian@gmail.com>
2023-04-03 12:11:58 -0300

    myFavorites finished

--- src/Components/myFavorites/styledMyFavorites.js
+++ src/Components/myFavorites/styledMyFavorites.js
@@ -21,3 +21,72 @@ export const MyFavoritesStyled = styled.div`
     padding: 20px 30px 40px;
   }
 `;
+
+export const NoFavorites = styled.div`
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media (min-width: 768px) {
+    margin-top: 50px;
+  }
+`;
+
+export const NoFavoritesContainer = styled.div`
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 50px 30px 50px;
+  width: 90%;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 8px;
+  max-width: 640px;
+  gap: 25px;
+
+  svg {
+    /* height: 78px; */
+    font-size: 60px;
+    fill: ${PRIMARY_COLOR};
+  }
+
+  h4 {
+    color: ${PRIMARY_COLOR};
+    font-size: 20px;
+    font-weight: 700;
+    margin-top: 10px;
+    text-align: center;
+  }
+
+  p {
+    color: ${PURPLE2};
+    font-weight: 500;
+    font-size: 16px;
+    text-align: center;
+  }
+
+  button {
+    width: 100%;
+    max-width: 205px;
+    border: none;
+    background-color: ${PRIMARY_COLOR};
+    color: #fff;
+    font-weight: 700;
+    border-radius: 5px;
+    font-size: 18px;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.12);
+    cursor: pointer;
+    transition: all 0.3s ease;
+    height: 40px;
+    margin-top: 20px;
+
+    &:hover {
+      background-color: #15a39a;
+    }
+  }
+
+  @media (min-width: 768px) {
+  }
+`;
--- src/Pages/MyFavorites.js
+++ src/Pages/MyFavorites.js
@@ -1,24 +1,53 @@
 import React, { useContext } from "react";
-import { MyFavoritesStyled } from "../Components/myFavorites/styledMyFavorites";
+import {
+  MyFavoritesStyled,
+  NoFavorites,
+  NoFavoritesContainer,
+} from "../Components/myFavorites/styledMyFavorites";
 import { ProductCardContainer } from "../Components/homePage/productSection/styledProductSection";
 import UserContext from "../context/user.context";
 import ProductCard from "../Components/homePage/productSection/ProductCard";
+import { BsHeartbreakFill as EmptyFavorites } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { routes } from "../Routes";
+
 
 export default function MyFavorites() {
   const { favs } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  }
 
   return (
-    <MyFavoritesStyled>
-      <h3>Mis favoritos</h3>
-      <ProductCardContainer>
-      {favs?.map((product) => (
-                <ProductCard
-                  key={product.id}
-                  product={product}
-                  isFav={true}
-                />
-              ))}
-      </ProductCardContainer>
-    </MyFavoritesStyled>
+    <>
+      <MyFavoritesStyled>
+        <h3>Mis favoritos</h3>
+
+        {favs.length === 0 ? (
+          <NoFavorites>
+            <NoFavoritesContainer>
+              <EmptyFavorites />
+              <h4>
+                Actualmente no hay alojamientos agregados a tu lista de
+                favoritos
+              </h4>
+              <p>
+                Para agregar alojamientos, navega a través de nuestro sitio y
+                marcá tus alojamientos favoritos con el botón ❤
+              </p>
+              <button onClick={goBack}>Volver</button>
+            </NoFavoritesContainer>
+          </NoFavorites>
+        ) : (
+          <ProductCardContainer>
+            {favs?.map((product) => (
+              <ProductCard key={product.id} product={product} isFav={true} />
+            ))}
+          </ProductCardContainer>
+        )}
+      </MyFavoritesStyled>
+    </>
   );
 }
